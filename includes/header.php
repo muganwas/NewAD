d8f9a601171dff35b4e9b6f73e9581c37cbf2cae
Muganwa <stevenmuganwa@live.com>
2017-04-25 18:14:00 +0300

    very important stage

---
+++ includes/header.php
@@ -0,0 +1,33 @@
+<link href="../css/main.css" rel="stylesheet" type="text/css">
+<div class="header_main">
+<a href="index.php"><div class="logo"><img src="images/logos/logo1.png" width="151" height="166" alt=""/></div></a>
+<div class="main_nav">
+<ul>
+<li>
+<a href="#">About</a>
+</li>
+<li>
+<a href="#">Work</a>
+</li>
+<li>
+Services
+<div class="ul_li">
+<ul>
+<li>
+<a href="#">Web Design</a>
+</li>
+<li>
+<a href="#">Web Developement</a>
+</li>
+<li>
+<a href="#">App Developement</a>
+</li>
+</ul>
+</div>
+</li>
+<li>
+<a href="#">Contact us</a>
+</li>
+</ul>
+</div>
+</div>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Alien Dynamics</title>
+<link href="favicon.png" rel="icon"/>
+<link href="css/main.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="main_top">
+<?php include_once"includes/header.php" ?>
+
+<div class="front">
+<div class="words_main">App Developement is us </div>
+<img src="images/icons/index_image.png" alt="web and mobile apps"/>
+</div>
+<a href="#"><div class="down_arrow"></div></a>
+</div>
+<main class="content_main">
+</main>
+</body>
+</html>
