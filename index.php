6a7d88a7c4fa685a75403dffcf57696e29c84cdb
Muganwa <stevenmuganwa@live.com>
2017-04-27 15:44:31 +0300

    better look

--- index.php
+++ index.php
@@ -23,16 +23,43 @@ $(document).ready(function(){
   </script>
 </head>
 <body>
-<div class="main_top">
 <?php include_once"includes/header.php" ?>
+<div class="main_top">
+
 
 <div class="front">
-<div class="words_main">App Developement is us </div>
+<div class="words_main">
+  <p>App Developement is us, </p>
+  <span class="words_minor">Pc or Mobile</span>
+</div>
 <img src="images/icons/index_image.png" alt="web and mobile apps"/>
 </div>
 <a href="#"><div class="down_arrow"></div></a>
 </div>
 <main class="content_main">
+<div class="featured_jobs">
+<h1>Featured Work</h1>
+
+<div class="featured_head_text">
+We are a Kampala based web developement company with brilliant web designers and foreward thinking mobile app developers, check out our latest work!
+</div>
+<div class="featured_left">
+<h2>kampala sun</h2>
+<a href="#"><img src="images/featured/kampala_sun.png" alt="kampala sun"/></a>
+</div>
+<div class="featured_right">
+<h2>vision group</h2>
+<a href="#"><img src="images/featured/vision_group.png" alt="kampala sun"/></a>
+</div>
+<div class="featured_left">
+<h2>vision Printing</h2>
+<a href="#"><img src="images/featured/vision_printing.png" alt="kampala sun"/></a>
+</div>
+<div class="featured_right">
+<h2>Acacia School</h2>
+<a href="#"><img src="images/featured/acacia_school.png" alt="kampala sun"/></a>
+</div>
+</div>
 </main>
 
 </body>
